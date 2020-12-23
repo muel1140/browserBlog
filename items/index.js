e7cc91104bba1fd039efc5b788a3d92ebe1dc452
muel1140 <76500014+muel1140@users.noreply.github.com>
2020-12-23 00:35:39 -0500

    Update index.js

--- items/index.js
+++ items/index.js
@@ -1,4 +1,4 @@
-export default [{"id": "item1", "fname":"item1.md", "specialImage":"/img/LOGO.png"},
+export default [{"id": "item1.md", "fname":"item1.md", "specialImage":"/img/LOGO.png"},
 {"id": "item2", "fname":"item2.md", "specialImage":"/img/FavouriteDay750x500_pdni4p.png"},
 {"id": "item3", "fname":"item3.md", "specialImage":"/img/twiliobot750x500_z7anzp.png"},
 
